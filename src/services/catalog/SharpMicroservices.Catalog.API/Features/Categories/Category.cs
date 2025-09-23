f9462e78c2977492e47c56a8895633e644c64fd0
Ehlullah Karakurt <ehlullah.fb@gmail.com>
2025-09-23 17:09:52 +0300

    created entities.

---
+++ src/services/catalog/SharpMicroservices.Catalog.API/Features/Categories/Category.cs
@@ -0,0 +1,10 @@
+﻿using SharpMicroservices.Catalog.API.Features.Courses;
+using SharpMicroservices.Catalog.API.Repositories;
+
+namespace SharpMicroservices.Catalog.API.Features.Categories;
+
+public class Category : BaseEntity
+{
+    public string Name { get; set; } = default!;
+    public List<Course>? Courses { get; set; }
+}
---
+++ src/services/catalog/SharpMicroservices.Catalog.API/Features/Courses/Feature.cs
@@ -0,0 +1,8 @@
+﻿namespace SharpMicroservices.Catalog.API.Features.Courses;
+
+public class Feature
+{
+    public int Duration { get; set; }
+    public float Rating { get; set; }
+    public string EducatorFullName { get; set; } = default!;
+}
