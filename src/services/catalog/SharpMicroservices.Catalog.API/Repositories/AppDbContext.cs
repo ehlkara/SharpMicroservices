e6b31518df0fa71e1d00c1aa0fb53cc1277b9a8f
Ehlullah Karakurt <ehlullah.fb@gmail.com>
2025-09-24 19:11:50 +0300

    created appDbContext and entity configuration

---
+++ src/services/catalog/SharpMicroservices.Catalog.API/Repositories/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MongoDB.EntityFrameworkCore.Extensions;
+using SharpMicroservices.Catalog.API.Features.Categories;
+using SharpMicroservices.Catalog.API.Features.Courses;
+using System.Reflection;
+
+namespace SharpMicroservices.Catalog.API.Repositories;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Course> Courses => Set<Course>();
+    public DbSet<Category> Categories => Set<Category>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}
