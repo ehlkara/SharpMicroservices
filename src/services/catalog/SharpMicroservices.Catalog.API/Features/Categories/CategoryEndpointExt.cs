cb0f5ab39dc551351582acfb86bbf834d829f352
Ehlullah Karakurt <ehlullah.fb@gmail.com>
2025-09-25 23:59:25 +0300

    .

--- src/services/catalog/SharpMicroservices.Catalog.API/Features/Categories/CategoryEndpointExt.cs
+++ src/services/catalog/SharpMicroservices.Catalog.API/Features/Categories/CategoryEndpointExt.cs
@@ -6,6 +6,6 @@ public static class CategoryEndpointExt
 {
     public static void AddCategoryGroupEndpointExt(this WebApplication app)
     {
-        app.MapGroup("api/categories").CreateCategoryGroupItemEndpoint().RequireAuthorization();
+        app.MapGroup("api/categories").CreateCategoryGroupItemEndpoint();
     }
 }
