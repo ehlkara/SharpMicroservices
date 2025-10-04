34e37129d698c058cb5a63ac78dd6c7110d78f1a
Ehlullah Karakurt <ehlullah.fb@gmail.com>
2025-10-04 10:04:58 +0300

    created addBasketItemCommand

---
+++ src/services/basket/SharpMicroservices.Basket.API/Features/Basket/AddBasketItem/AddBasketItemCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace SharpMicroservices.Basket.API.Features.Basket.AddBasketItem;
+
+public record AddBasketItemCommand(Guid CourseId, string CourseName, decimal CoursePrice, string CourseImageUrl);
