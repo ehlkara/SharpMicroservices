12864c1fd5e9806a7d3d67cec9ea070ebd7e56de
Ehlullah Karakurt <ehlullah.fb@gmail.com>
2025-10-09 17:47:08 +0300

    created orderItem Entity

---
+++ src/services/order/core/SharpMicroservices.Order.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace SharpMicroservices.Order.Domain.Entities;
+
+public class BaseEntity<TEntity>
+{
+    public TEntity Id { get; set; } = default!;
+}
---
+++ src/services/order/core/SharpMicroservices.Order.Domain/Entities/OrderItem.cs
@@ -0,0 +1,49 @@
+﻿namespace SharpMicroservices.Order.Domain.Entities;
+
+public class OrderItem : BaseEntity<int>
+{
+    public Guid ProductId { get; set; }
+    public string ProductName { get; set; } = default!;
+    public decimal UnitPrice { get; set; }
+
+
+    public void SetItem(Guid productId, string productName, decimal unitPrice)
+    {
+        if(string.IsNullOrWhiteSpace(ProductName))
+        {
+            throw new ArgumentException("Product name cannot be empty.", nameof(productName));
+        }
+
+        if(unitPrice < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(unitPrice), "Unit price cannot be negative.");
+        }
+
+        ProductId = productId;
+        ProductName = productName;
+        UnitPrice = unitPrice;
+    }
+
+    public void UpdatePrice(decimal newPrice)
+    {
+        if (newPrice < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(newPrice), "Unit price cannot be negative.");
+        }
+        UnitPrice = newPrice;
+    }
+
+    public void ApplyDiscount(double discountPercentage)
+    {
+        if (discountPercentage < 0 || discountPercentage > 100)
+        {
+            throw new ArgumentOutOfRangeException(nameof(discountPercentage), "Discount percentage must be between 0 and 100.");
+        }
+        UnitPrice -= UnitPrice * ((decimal)discountPercentage / 100);
+    }
+
+    public bool IsSameItem(OrderItem orderItem)
+    {
+        return this.ProductId == orderItem.ProductId;
+    }
+}
