53ea476fecafee5612da0bbf509728a83a516f6f
Ehlullah Karakurt <ehlullah.fb@gmail.com>
2025-10-11 23:27:59 +0300

    created migration

--- src/services/order/api/SharpMicroservices.Order.API/Program.cs
+++ src/services/order/api/SharpMicroservices.Order.API/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using SharpMicroservices.Order.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+});
 
 var app = builder.Build();
 
