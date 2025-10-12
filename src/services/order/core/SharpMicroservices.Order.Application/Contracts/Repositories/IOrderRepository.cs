63293f0cec765c898447892217328aabccc6d898
Ehlullah Karakurt <ehlullah.fb@gmail.com>
2025-10-12 15:44:58 +0300

    created GetOrders method in OrderRepository

--- src/services/order/core/SharpMicroservices.Order.Application/Contracts/Repositories/IOrderRepository.cs
+++ src/services/order/core/SharpMicroservices.Order.Application/Contracts/Repositories/IOrderRepository.cs
@@ -2,4 +2,5 @@
 
 public interface IOrderRepository : IGenericRepository<Guid, Domain.Entities.Order>
 {
+    public Task<List<Domain.Entities.Order>> GetOrdersByBuyerIdAsync(Guid buyerId);
 }
--- src/services/order/infrastructure/SharpMicroservices.Order.Persistence/Repositories/OrderRepository.cs
+++ src/services/order/infrastructure/SharpMicroservices.Order.Persistence/Repositories/OrderRepository.cs
@@ -1,7 +1,12 @@
-﻿using SharpMicroservices.Order.Application.Contracts.Repositories;
+﻿using Microsoft.EntityFrameworkCore;
+using SharpMicroservices.Order.Application.Contracts.Repositories;
 
 namespace SharpMicroservices.Order.Persistence.Repositories;
 
 public class OrderRepository(AppDbContext context) : GenericRepository<Guid, Domain.Entities.Order>(context), IOrderRepository
 {
+    public Task<List<Domain.Entities.Order>> GetOrdersByBuyerIdAsync(Guid buyerId)
+    {
+        return context.Orders.Include(x => x.OrderItems).Where(x => x.BuyerId == buyerId).OrderByDescending(x => x.Created).ToListAsync();
+    }
 }
