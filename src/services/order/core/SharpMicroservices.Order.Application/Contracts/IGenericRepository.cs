971da77820e82cac20ec6550aa819d9f5cf3b46c
Ehlullah Karakurt <ehlullah.fb@gmail.com>
2025-10-12 11:10:03 +0300

    created Igeneric repository

---
+++ src/services/order/core/SharpMicroservices.Order.Application/Contracts/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace SharpMicroservices.Order.Application.Contracts;
+
+public interface IGenericRepository<TId, TEntity> where TEntity : class where TId : struct
+{
+    public Task<bool> AnyAsync(TId id);
+    public Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate);
+    Task<List<TEntity>> GetAllAsync();
+    Task<List<TEntity>> GetAllPagedAsync(int pageNumber, int pageSize);
+    ValueTask<TEntity> GetByIdAsync(TId id);
+    IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
+    void Add(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+}
