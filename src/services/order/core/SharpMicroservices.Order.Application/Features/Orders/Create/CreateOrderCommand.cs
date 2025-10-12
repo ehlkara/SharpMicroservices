daece430798a37c3f95791050738677573f8f6d3
Ehlullah Karakurt <ehlullah.fb@gmail.com>
2025-10-12 13:46:27 +0300

    created CreateOrderCommandHandler

---
+++ src/services/order/core/SharpMicroservices.Order.Application/Features/Orders/Create/CreateOrderCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace SharpMicroservices.Order.Application.Features.Orders.Create;
+
+public record CreateOrderCommand(float? discountRate, AddressDto Address, PaymentDto Payment, List<OrderItemDto> Items);
+
+public record AddressDto(string Province, string District, string Street, string ZipCode, string Line);
+
+public record PaymentDto(string CardNumber, string CardHolderName, string Expiration, string Cvc, decimal Amount);
+
+public record OrderItemDto(Guid ProductId, string ProductName, decimal UnitPrice);
