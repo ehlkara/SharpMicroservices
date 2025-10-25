d348dfaf840bc876b8ed2aa326df17f0aaead615
Ehlullah Karakurt <ehlullah.fb@gmail.com>
2025-10-25 15:40:45 +0300

    security gateway configuration

--- src/services/gateway/SharpMicroservices.Gateway/Program.cs
+++ src/services/gateway/SharpMicroservices.Gateway/Program.cs
@@ -1,8 +1,16 @@
+using SharpMicroservices.Shared.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddAuthenticationAndAuthorizationExt(builder.Configuration);
+
 var app = builder.Build();
 app.MapReverseProxy();
 app.MapGet("/", () => "YARP (Gateway)");
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 await app.RunAsync();
